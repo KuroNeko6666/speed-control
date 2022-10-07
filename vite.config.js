05354b7e6b34c7af9bb7fbdcb6416729f3a6230e
“Agung <agungpriyatno666@email.com>
2022-10-07 13:18:24 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,8 @@ import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     server: {
-        hmr: {
-            host: 'localhost',
-        },
+        host: true,
+
     },
     plugins: [
         laravel({
