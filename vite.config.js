cdfea07e0a28f1b165601a297ff1db9f2c498998
“Agung <agungpriyatno666@email.com>
2022-10-07 10:44:52 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,14 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/scss/sb-admin-2.scss' ,'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/assets/scss/sb-admin-2.scss' ,'resources/js/app.js'],
             refresh: true,
         }),
     ],
     server: {
-        port: 8000,
+        port: 8001,
+        cors: false,
+        hmr: false,
+        https: false,
     }
 });
