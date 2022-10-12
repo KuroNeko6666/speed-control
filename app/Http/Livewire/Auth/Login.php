aa22253f6fa6726a393c8edf224a0b15f51ed979
“Agung <agungpriyatno666@email.com>
2022-10-12 21:58:01 +0700

    update

--- app/Http/Livewire/Auth/Login.php
+++ app/Http/Livewire/Auth/Login.php
@@ -9,7 +9,7 @@ class Login extends Component
     public $email, $password;
 
     protected $rules = [
-        'email' => 'required',
+        'email' => 'required|email',
         'password' => 'required',
     ];
 
